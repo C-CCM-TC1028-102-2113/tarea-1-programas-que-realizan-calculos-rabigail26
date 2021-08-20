e7a1b4d3726f9c7abe2b6ef71d07503fca10470c
rabigail26 <89092963+rabigail26@users.noreply.github.com>
2021-08-20 15:19:20 -0500

    Update exercise.py

--- assignments/18CuentaBancaria/src/exercise.py
+++ assignments/18CuentaBancaria/src/exercise.py
@@ -1,6 +1,18 @@
 def main():
     #escribe tu código abajo de esta línea
     pass
-
+print ("Dame el saldo del mes anterior")
+mesanterior = float (input())
+print ("Dame los ingresos")
+ingresos = float (input())
+print ("Dame los egresos:")
+egresos = float(input())
+print ("Dame el numero de cheques expedidos")
+cheques = int(input())
+saldo = (mesanterior + ingresos -egresos) 
+intereses=(((saldo -(saldo *(0.075))) + (cheques *13)) )
+round(intereses,2)
+print ("El saldo final es:")
+print (intereses)
 if __name__ == '__main__':
     main()
