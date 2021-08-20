9dec18ad82b6db9614232513290234a8a12a6115
rabigail26 <89092963+rabigail26@users.noreply.github.com>
2021-08-20 15:15:22 -0500

    Update exercise.py

--- assignments/08Caracol/src/exercise.py
+++ assignments/08Caracol/src/exercise.py
@@ -1,6 +1,16 @@
 def main():
     #escribe tu código abajo de esta línea
     pass
+#Caracol
 
+
+print ("Dame los minutos: ")
+
+tiempo = float(input())
+vel = 6
+distancia = tiempo * vel
+print ("Los centrimetros reccorridos son:  ")
+
+print (distancia, "cm/min")
 if __name__ == '__main__':
     main()
