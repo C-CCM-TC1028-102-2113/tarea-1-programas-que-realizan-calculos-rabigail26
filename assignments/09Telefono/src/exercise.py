2e717cb42aad63fccfb6823b69b798b73406bfcf
rabigail26 <89092963+rabigail26@users.noreply.github.com>
2021-08-20 15:16:37 -0500

    Update exercise.py

--- assignments/09Telefono/src/exercise.py
+++ assignments/09Telefono/src/exercise.py
@@ -2,6 +2,16 @@ def main():
     #escribe tu código abajo de esta línea
     #Leer los datos
     pass
+print ('Dame el número de mensajes:')
+mensajes= int(input())
+print ('Dame el número de megas:')
+megas= float(input())
+print ('Dame el número de minutos:')
+minutos= int(input())
+
+Costo=(mensajes*0.80)+(megas*0.80)+(minutos*0.80)
+print ('El costo mensual es:')
+print(Costo)
 
 
 if __name__ == '__main__':
