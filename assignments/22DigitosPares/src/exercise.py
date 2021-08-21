296b9e327f88d402703b010a2529a68088de6d1c
rabigail26 <89092963+rabigail26@users.noreply.github.com>
2021-08-21 10:15:47 -0500

    Update exercise.py

--- assignments/22DigitosPares/src/exercise.py
+++ assignments/22DigitosPares/src/exercise.py
@@ -2,6 +2,17 @@ def main():
     #escribe tu código abajo de esta línea
     pass
 
+#Numeros pares
+valores = int(input("Inserte los digitos"))
+if valores <0 :
+    print("¡Error!")
+else:
+    pares = 0
+if valores % 2 == 0:
+    pares += 1
+    print(f"Ha escrito {pares} números pares y {valores - pares} números impares.")
+
+
 
 if __name__ == '__main__':
     main()
